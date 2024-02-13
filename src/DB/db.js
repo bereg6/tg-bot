1cac500f305ca156c32d5586b9f21786df31ed74
bereg6 <bereg6@gmail.com>
2024-02-13 18:32:49 +0200

    ch

--- src/DB/db.js
+++ src/DB/db.js
@@ -5,23 +5,32 @@ const data = [];
 export function addToDB(){
     const gameName = document.querySelector('#gameName').value;
     const price = document.querySelector('#price').value;
-    if(gameName === ''){
-        const label = document.querySelectorAll('label')[0];
-        console.log(document.querySelectorAll('label'));
-        label.style = 'color: red;';
-        label.innerText = 'Game name field can`t be empty'
-    } else if(price === ''){
-        const label = document.querySelectorAll('label')[1];
-        label.style = 'color: red;';
-        label.innerText = 'Price field can`t be empty'
+    if(gameName === '' || price === ''){
+        if(gameName === ''){
+           const label = document.querySelectorAll('label')[0];
+           label.style = 'color: red;';
+           label.innerText = 'Game name field can`t be empty'
+        }
+        if(price === ''){
+           const label = document.querySelectorAll('label')[1];
+           label.style = 'color: red;';
+           label.innerText = 'Price field can`t be empty'
+        }
     } else{
         const newElement = {
             Game_name: gameName,
             Price: price,
         };
+        const label = document.querySelectorAll('label');
+        label.forEach((el) => {
+            el.style = 'color: #000;';
+        })
         document.querySelector('#gameName').value = '';
+        label[0].innerText = 'Game name:';
         document.querySelector('#price').value = '';
+        label[1].innerText = 'Price:';
         console.log(data);
+        // console.log(document.querySelector('#price'));
         return data.splice(data.length, 0, newElement);
     }
   }
